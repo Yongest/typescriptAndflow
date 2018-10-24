3599615cb84b42ec5ab57ac36d7cef340fb1e91c
yongest <1046788379@qq.com>
2018-10-24 14:27:24 +0800

    添加flow and typescript笔记

---
+++ index.js
@@ -0,0 +1,11 @@
+/*@flow*/
+
+// https://blog.csdn.net/fen747042796/article/details/68945984
+// npm init -y
+//npm install flow-bin -D
+// npm run flow init   // 中文路径没有生存相关的配置文件
+// npm run flow
+
+var a /* : number */ = 1;
+
+a = 2;
